9bfcc175ece4db8898a61e0cee515eeb79b6ffef
A-F-2 <dualac26@gmail.com>
2024-07-18 14:04:31 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+﻿let $ = document
+
+let inputElem = $.querySelector("#input")
+
+let deletorElem = $.querySelectorAll(".fa.fa-trash-o.delete")
+
+let ulElem = $.querySelector("#ul")
+
+let formElem = $.querySelector("#form")
+
+formElem.addEventListener("submit" , function (event) {
+    event.preventDefault()
+})
+
+inputElem.addEventListener("keydown" , function (event) {
+    if(inputElem.value !="") {
+            if (event.key === "Enter") {
+                let spanElem = $.createElement("span")
+                let iElem = $.createElement("i")
+                let liElem = $.createElement("li")
+                iElem.setAttribute("class" , "fa fa-trash-o delete")
+                liElem.setAttribute("class" , "list-group-item d-flex justify-content-between align-items-center")
+                iElem.addEventListener("click" , function (event) {
+            event.target.parentElement.remove()
+                })
+                spanElem.innerHTML = inputElem.value
+                liElem.append(spanElem)
+                liElem.append(iElem)
+                ulElem.append(liElem)
+                inputElem.value = ""
+            }
+    }   else {
+        inputElem.value = ""
+    }
+})
+
+deletorElem.forEach(function (deletor) {
+    deletor.addEventListener("click" , function (event) {
+        event.target.parentElement.remove()
+    })
+})
